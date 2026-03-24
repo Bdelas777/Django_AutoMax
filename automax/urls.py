1fcc859090cdcbc8b1eb8703bdd77e88edced123
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2026-03-24 12:48:45 -0600

    FIX Media routing

--- automax/urls.py
+++ automax/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main import urls as main_urls
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
