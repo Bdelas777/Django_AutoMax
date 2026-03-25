ceb572dc97ed2032454869cbe2f65073525fee29
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2026-03-25 10:15:50 -0600

    feat add messages

--- automax/settings.py
+++ automax/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -104,6 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# Messages Settings
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
