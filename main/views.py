417d874b8ed0d87fac7a32b7e2692e1f4819bc02
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2026-03-26 13:31:43 -0600

    feat add Profile and decorators

--- main/views.py
+++ main/views.py
@@ -1,15 +1,14 @@
-
-from django.http import JsonResponse
-from django.shortcuts import redirect, render, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.utils.decorators import method_decorator
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views import View
 
-from .models import LikedListing, Listing
-from .forms import ListingForm
-from users.forms import LocationForm
-from .filters import ListingFilter
-
+from main.models import Listing, LikedListing
+from .forms import UserForm, ProfileForm, LocationForm
 # Create your views here.
 
 
@@ -58,7 +57,6 @@ def list_view(request):
         location_form = LocationForm()
     return render(request, 'views/list.html', {'listing_form': listing_form, 'location_form': location_form, })
 
-
 @login_required
 def listing_view(request, id):
     try:
@@ -69,3 +67,43 @@ def listing_view(request, id):
     except Exception as e:
         messages.error(request, f'Invalid UID {id} was provided for listing.')
         return redirect('home')
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+
+    def get(self, request):
+        user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(
+            profile=request.user.profile).all()
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+        location_form = LocationForm(instance=request.user.profile.location)
+        return render(request, 'views/profile.html', {'user_form': user_form,
+                                                      'profile_form': profile_form,
+                                                      'location_form': location_form,
+                                                      'user_listings': user_listings,
+                                                      'user_liked_listings': user_liked_listings, })
+
+    def post(self, request):
+        user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(
+            profile=request.user.profile).all()
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        location_form = LocationForm(
+            request.POST, instance=request.user.profile.location)
+        if user_form.is_valid() and profile_form.is_valid() and location_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            location_form.save()
+            messages.success(request, 'Profile Updated Successfully!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Error Updating Profile!')
+        return render(request, 'views/profile.html', {'user_form': user_form,
+                                                      'profile_form': profile_form,
+                                                      'location_form': location_form,
+                                                      'user_listings': user_listings,
+                                                      'user_liked_listings': user_liked_listings, })
--- users/views.py
+++ users/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views import View
+
+from main.models import Listing, LikedListing
+from .forms import UserForm, ProfileForm, LocationForm
+
 # Create your views here.
 
 def login_view(request):
@@ -53,3 +57,42 @@ class RegisterView(View):
             messages.error(request, f'An error occured trying to register.')
             return render(request, 'views/register.html', {'register_form': register_form})
 
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+
+    def get(self, request):
+        user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(
+            profile=request.user.profile).all()
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+        location_form = LocationForm(instance=request.user.profile.location)
+        return render(request, 'views/profile.html', {'user_form': user_form,
+                                                      'profile_form': profile_form,
+                                                      'location_form': location_form,
+                                                      'user_listings': user_listings,
+                                                      'user_liked_listings': user_liked_listings, })
+
+    def post(self, request):
+        user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(
+            profile=request.user.profile).all()
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        location_form = LocationForm(
+            request.POST, instance=request.user.profile.location)
+        if user_form.is_valid() and profile_form.is_valid() and location_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            location_form.save()
+            messages.success(request, 'Profile Updated Successfully!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Error Updating Profile!')
+        return render(request, 'views/profile.html', {'user_form': user_form,
+                                                      'profile_form': profile_form,
+                                                      'location_form': location_form,
+                                                      'user_listings': user_listings,
+                                                      'user_liked_listings': user_liked_listings, })
