807c1bc707806d2c09d4daa3d84de5e9f0f1097d
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2026-03-26 12:44:41 -0600

    feat add listing

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Listing
+from .forms import ListingForm, LocationForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -12,10 +14,36 @@ def landing_page(request):
     return render(request, 'views/main.html')
 
 @login_required
-@login_required
 def home_view(request):
     listings = Listing.objects.all()
     context = {
         'listings': listings,
     }
     return render(request, "views/home.html", context)
+
+
+@login_required
+def list_view(request):
+    if request.method == 'POST':
+        try:
+            listing_form = ListingForm(request.POST, request.FILES)
+            location_form = LocationForm(request.POST, )
+            if listing_form.is_valid() and location_form.is_valid():
+                listing = listing_form.save(commit=False)
+                listing_location = location_form.save()
+                listing.seller = request.user.profile
+                listing.location = listing_location
+                listing.save()
+                messages.info(
+                    request, f'{listing.model} Listing Posted Successfully!')
+                return redirect('home')
+            else:
+                raise Exception()
+        except Exception as e:
+            print(e)
+            messages.error(
+                request, 'An error occured while posting the listing.')
+    elif request.method == 'GET':
+        listing_form = ListingForm()
+        location_form = LocationForm()
+    return render(request, 'views/list.html', {'listing_form': listing_form, 'location_form': location_form, })
